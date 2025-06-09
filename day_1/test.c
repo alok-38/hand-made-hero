f8f461720eb2255aa416e0fbb6e1d40875514e46
Alokananda Y <plantsfortrees@gmail.com>
2025-06-09 12:14:33 +0530

    Writing my first c program using the windows API

--- day_1/test.c
+++ day_1/test.c
@@ -1,6 +1,25 @@
-﻿#include <stdio.h>
+﻿#include <windows.h>
+#include <stdio.h>
 
-int main() {
-    printf("Hello, world!\n");
+void foo(void)
+{
+    OutputDebugStringA("This is the first thing we have actually printed.\n");
+}
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR ipCmdLine,
+    int nCmdShow)
+{
+    foo();
+
+    // Example: Get the last error code
+    DWORD err = GetLastError();
+
+    char buffer[256];
+    wsprintfA(buffer, "Last error code: %lu\n", err);
+    OutputDebugStringA(buffer);
+
     return 0;
 }
---
+++ day_1/windows.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int main()
+{
+    MessageBox(NULL, "Hello, Windows!", "My First Windows App", MB_OK);
+    return 0;
+}
